3b0a20e30b708ddf9b0cd9e91decfc0bcfc2783d
Matty Smith <matthewcsmith18@gmail.com>
2019-03-22 01:03:51 -0400

    custom

--- custom-api/index.js
+++ custom-api/index.js
@@ -1,8 +1,53 @@
 const request = require('request-promise');
+const cheerio = require('cheerio');
 
-async function scrape() {
-	const url = 'https://mattermark.com/';
-	const json = await request.get(url);
+const url = 'https://mattermark.com/app/api/';
+
+const scrapeResults = [];
+
+async function scrapeJobHeader() {
+	try {
+		const htmlResult = await request.get(url);
+		const $ = await cheerio.load(htmlResult);
+
+		$('.result-info').each((index, element) => {
+			const resultTitle = $(element).children('.result-title');
+			const title = resultTitle.text();
+			const url = resultTitle.attr('href');
+			const datePosted = new Date($(element).children('time').attr('datetime'));
+			const neighborhood = $(element).find('.result-hood').text();
+			const scrapeResult = { title, url, datePosted, neighborhood };
+			scrapeResults.push(scrapeResult);
+		});
+		return scrapeResults;
+	} catch (err) {
+		console.error(err);
+	}
 }
 
-scrape();
+async function scrapeDescription(jobsWithHeaders) {
+	return await Promise.all(
+		jobsWithHeaders.map(async job => {
+			try {
+				const htmlResult = await request.get(job.url);
+				const $ = await cheerio.load(htmlResult);
+				$('.print-qrcode-container').remove();
+				job.description = $('#postingbody').text();
+				job.address = $('div.mapaddress').text();
+				const compensationText = $('.attrgroup').children().first().text();
+				job.compensation = compensationText.replace('compensation: ', '');
+				return job;
+			} catch (error) {
+				console.error(error);
+			}
+		})
+	);
+}
+
+async function scrapeCraigslist() {
+	const jobsWithHeaders = await scrapeJobHeader();
+	const jobsFullData = await scrapeDescription(jobsWithHeaders);
+	console.log(jobsFullData);
+}
+
+scrapeCraigslist();
