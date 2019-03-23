bcd9e0f908049d807e72cfdcedee054d045ec3ab
Matty Smith <matthewcsmith18@gmail.com>
2019-03-23 15:09:15 -0400

    puppeteer

--- airbnb/index.js
+++ airbnb/index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+// Sample Schema
 const sample = {
   guests: 1,
   bedrooms: 1,
@@ -14,9 +15,14 @@ let browser;
 async function scrapeHomesInIndexPage(url) {
   try {
     const page = await browser.newPage();
-    await page.goto(url);
+    /*   
+    * CONSIDER NAVIGATION FINISHED WHEN THERE ARE NO MORE
+    * THAN 2 NETWORK CONNECTIONS FOR AT LEAST 500MS.
+    */
+    await page.goto(url, { waitUntil: 'networkidle2' });
     const html = await page.evaluate(() => document.body.innerHTML);
     const $ = await cheerio.load(html);
+
     const homes = $("[itemprop='url']")
       .map((i, element) => 'https://' + $(element).attr('content'))
       .get();
@@ -27,6 +33,7 @@ async function scrapeHomesInIndexPage(url) {
 }
 
 async function scrapeDescriptionPage(url, page) {
+  let roomText;
   try {
     /*   
     * CONSIDER NAVIGATION FINISHED WHEN THERE ARE NO MORE
@@ -35,14 +42,43 @@ async function scrapeDescriptionPage(url, page) {
     await page.goto(url, { waitUntil: 'networkidle2' });
     const html = await page.evaluate(() => document.body.innerHTML);
     const $ = await cheerio.load(html);
+    roomText = $('#room').text();
+
     const pricePerNight = $(
       '#room > div > div > div > div > div > div > div > div > div > div:nth-child(1) > div > div > div > div > div > div > div > div:nth-child(1) > span > div > div > span > span'
     ).text();
-    console.log(pricePerNight);
+
+    // HAD TO USES REGULAR EXPRESSIONS TO EXTRACT INFORMATION.
+    const guestsAllowed = returnMatches(roomText, /\d+ guest/);
+    const bedrooms = returnMatches(roomText, /\d+ (studio )?bedroom/);
+    const beds = returnMatches(roomText, /\d+ bed/);
+    const baths = returnMatches(roomText, /\d+ (shared )?bath/);
+
+    return {
+      url,
+      guestsAllowed,
+      bedrooms,
+      beds,
+      baths,
+      pricePerNight
+    };
   } catch (error) {
+    console.error(roomText);
+    console.error(url);
     console.error(error);
   }
 }
+
+const returnMatches = (roomText, regex) => {
+  const regExMatches = roomText.match(regex);
+  let result = 'N/A';
+  if (regExMatches != null) {
+    result = regExMatches[0];
+  } else {
+    throw `No regex matches found for ${regex}`;
+  }
+  return result;
+};
 
 async function main() {
   browser = await puppeteer.launch({ headless: false });
@@ -50,9 +86,35 @@ async function main() {
   const homes = await scrapeHomesInIndexPage(
     'https://www.airbnb.ca/s/Burlington--VT--United-States/homes?refinement_paths%5B%5D=%2Fhomes&query=Burlington%2C%20VT%2C%20United%20States&place_id=ChIJ5VWbtlV6ykwRjkkOchnlX8M&allow_override%5B%5D=&s_tag=iYw3W206'
   );
+  console.log(homes);
   for (let i = 0; i < homes.length; i++) {
-    await scrapeDescriptionPage(homes[i], descriptionPage);
+    const result = await scrapeDescriptionPage(homes[i], descriptionPage);
+    console.log(result);
   }
 }
 
 main();
+
+/* const guestMatches = roomText.match(/\d+ guest/);
+    let guestsAllowed = 'N/A';
+    if (guestMatches != null) {
+      guestsAllowed = guestMatches[0];
+    }
+
+    const bedroomMatches = roomText.match(/\d+ bedroom/);
+    let totalBedrooms = 'N/A';
+    if (bedroomMatches != null) {
+      totalBedrooms = bedroomMatches[0];
+    }
+
+    const bedMatches = roomText.match(/\d+ bed/);
+    let totalBeds = 'N/A';
+    if (bedMatches != null) {
+      totalBeds = bedMatches[0];
+    }
+
+    const bathMatches = roomText.match(/\d+ bath/);
+    let totalBaths = 'N/A';
+    if (bathMatches != null) {
+      totalBaths = bathMatches[0];
+    } */
